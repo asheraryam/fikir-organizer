185d203cc1bfe9c6893580cf04656e7fc6f8d1ea
asheraryam <asheraryam@gmail.com>
2020-05-30 20:10:46 +0300

    Update obsolete project name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Thinker")]
+[assembly: AssemblyTitle("Fikir")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
